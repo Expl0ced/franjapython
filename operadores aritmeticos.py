ce306f49fc687cdfe0a38443ef42ace92f8da7c7
exploced <46355681+exploced@users.noreply.github.com>
2022-04-13 17:07:55 -0400

    primera clase practica

---
+++ operadores aritmeticos.py
@@ -0,0 +1,31 @@
+print("Suma")
+print(2+3)
+
+print("Resta")
+print(5-3)
+
+print("Multiplicacion")
+print(5*3)
+
+print("Division")
+print(21/5)
+
+print("Division Entera")
+print(21 // 5)
+
+print("Division Modulo")
+print(21 % 5)
+
+print("Exponente")
+print(21 ** 5)
+
+print("Raiz cuadrada con POW")
+print(pow(21,0.5))
+
+import math
+print("Raiz cuadrada con MATH.SQRT")
+print(math.sqrt(21))
+
+
+#Regla de procedencia
+print(5+(5*2))
---
+++ variables.py
@@ -0,0 +1,17 @@
+numero = 3
+print(numero)
+
+numero1=5
+numero2=3
+
+print(numero1+numero2)
+
+
+numero=numero1+numero2
+print(numero)
+
+#las variables no pueden contener espacios, debe ser todo junto
+#las variables no pueden empezar por un numero
+#no pueden contener caracteres especiales
+#siempre escribir variables en minuscula
+#si tienen mas de una palabra deben ir separadas con un guien bajo (_)
